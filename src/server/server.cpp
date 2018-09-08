49efe20ec55c652a6fb52777e1adc7b1c55be0d7
Liu Xiaoyi <circuitcoder0@gmail.com>
2018-09-09 04:55:01 +0800

    Basic server setup

---
+++ src/server/server.cpp
@@ -0,0 +1,92 @@
+#include "server.h"
+
+using namespace std;
+
+Side negateSide(Side s) {
+  if(s == RED) return BLACK;
+  else return RED;
+}
+
+Server::Server(quint16 port, int timeout) {
+  this->server = new QTcpServer(this);
+  this->server->listen(QHostAddress::Any, port);
+
+  this->timer = new QTimer(this);
+  this->timer->setInterval(timeout * 1000);
+
+  this->localSide = RED;
+
+  // TODO: setup board
+}
+
+Response Server::localApply(Request req) {
+  return this->apply(req, this->localSide);
+}
+
+Response Server::apply(Request req, Side side) {
+  if(req.has_resign()) {
+    Call call;
+    call.set_winner(negateSide(side));
+    call.set_cause(Call::RESIGN);
+
+    this->call(call);
+
+    Response resp;
+    resp.set_success(true);
+    return resp;
+  } else {
+    // Movement
+    const auto movement = req.movement();
+
+    Response resp;
+
+    unique_lock<shared_mutex> lock(boardMutex);
+    // Search through the board
+    // TODO: validation
+    for(auto i = this->board.begin(); i != this->board.end(); ++i)
+      if(i->x() == movement.to_x() && i->y() == movement.to_y()) {
+        const auto removed = *i;
+        this->board.erase(i);
+        if(removed.type() == Piece::GENERAL) {
+          Call call;
+          call.set_winner(negateSide(removed.side()));
+          call.set_cause(Call::CHECKMATE);
+
+          this->call(call);
+        }
+        break;
+      }
+
+    for(auto i = this->board.begin(); i != this->board.end(); ++i)
+      if(i->x() == movement.from_x() && i->y() == movement.from_y()) {
+        i->set_x(movement.to_x());
+        i->set_y(movement.to_y());
+      }
+    
+    this->syncBoard();
+
+    resp.set_success(true);
+    return resp;
+  }
+}
+
+void Server::syncBoard() {
+  Sync sync;
+  shared_lock<shared_mutex> lock(boardMutex);
+  Board *b = sync.mutable_board();
+
+  for(const auto &piece : this->board)
+    *b->add_pieces() = piece;
+  
+  emit localSync(sync);
+  // TODO: remote sync
+}
+
+void Server::call(Call call) {
+  Sync sync;
+  *sync.mutable_call() = call;
+  emit localSync(sync);
+  // TODO: remote sync
+}
+
+// TODO: timeout
---
+++ src/server/server.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QWidget>
+#include <QTcpServer>
+#include <QTimer>
+#include <vector>
+#include <shared_mutex>
+#include "proto/msg.pb.h"
+
+class Server : QWidget {
+  Q_OBJECT
+
+  public:
+    Server(quint16 port, int timeout = 60);
+    Response localApply(Request req);
+
+  signals:
+    void localSync(Sync sync);
+
+  private:
+    QTcpServer *server;
+    QTimer *timer;
+    std::vector<Piece> board;
+    Side localSide;
+
+    std::shared_mutex boardMutex;
+
+    Response apply(Request req, Side side);
+    void syncBoard();
+    void call(Call call);
+};
